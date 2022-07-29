0efb8631b6a5b2d939489fac4c7c2aff9372f0bd
LagrangeVI <amenxd01@gmail.com>
2022-07-29 22:21:59 +0800

    add axios wrapper

---
+++ useAxios.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+
+const base_url = "http://localhost:3000/api"
+
+exports.useAxios = async (method, path, req, ) => {
+
+  const ENDPOINT = `${base_url}${path}`
+
+  let config = {
+    url: ENDPOINT,
+    method: method,
+    validateStatus: function (status) {
+      return status >= 200 && status < 600
+    },
+  }
+	
+	if (req) {
+    config.data = req
+	}
+	
+  try {
+
+    let response = await axios(config)
+    let code = response.status
+    let data = response.data
+
+    if (code >= 200 && code < 300) {
+      console.log(`\nSUCCESS:  ${code} ${path} \n${JSON.stringify(req)}\n\n`)
+		} else {
+			console.log(`\nFAILED:  ${code} ${path} \n${JSON.stringify(req)}\n\n`)
+		}
+		console.log(JSON.stringify(data))
+	} catch (error) {
+		console.log(`\nERROR:  ${error} ${path} ${req}`)
+  }
+}
+
